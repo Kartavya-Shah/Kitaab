fc45f981200fbe54631c295de0aee70fa0707a01
ominema12 <ominema12@gmail.com>
2024-01-19 22:25:38 +0530

    home

--- client/src/routes/Home/home.jsx
+++ client/src/routes/Home/home.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      
+      <h1>Home</h1>
+      <p>Miniature Vite + React demo website.</p>
+      <p>It uses NodeJs, Express & MongoDB as backend!</p>
     </div> 
   )
 }
