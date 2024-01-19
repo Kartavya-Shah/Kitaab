978155785637ac8c9dcbc13b716b2cb6d1cd5a74
ominema12 <ominema12@gmail.com>
2024-01-19 20:45:55 +0530

    created routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
+ import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+       <Router>
+        Header
+        <Routes>
+          <Route>
+
+          </Route>
+        </Routes>
+        Footer
+       </Router>
     </>
   )
 }
