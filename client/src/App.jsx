aa17276f53f2b6772d0757a6128276c89176067f
ominema12 <ominema12@gmail.com>
2024-02-05 10:24:44 +0530

    error correcting try

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,12 @@
-//  import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
-// import Home from "./routes/Home/home";
-// import About from "./routes/About/about";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer"; 
-// import Book from "./routes/Book/book";
-
-// function App() {
-
-//   return (
-//     <>
-//        <Router>
-//         <Header/>
-//         <Routes>
-//           <Route path="/" element={<Home/>}/>
-//           <Route path="/about" element={<About/>}/>
-//           <Route path="/books" element={ <Book/> } />
-//         </Routes>
-//         <Footer/>
-//        </Router>
-//     </>
-//   )
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home/home";
 import About from "./routes/About/about";
 import Book from "./routes/Book/book";
+import SingleBook from "./routes/Book/singleBook";
+import CreateBook from "./routes/Book/createBook";
+import EditBook from "./routes/Book/editBook";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import SingleBook from "./routes/Book/singleBook";
-import createBook from "./routes/Book/createBook";
 
 function App() {
   return (
@@ -42,10 +17,11 @@ function App() {
           <Route path="/" element={ <Home/> } />
           <Route path="/about" element={ <About/> } />
           <Route path="/books" element={ <Book/> } />
-          <Route path="/books/:slug" element={<SingleBook/>} />
-          <Route path="/createbook" element={<createBook/>} />
+          <Route path="/books/:slug" element={ <SingleBook/> } />
+          <Route path="/createbook" element={ <CreateBook/> } />
+          <Route path="/editbook/:slug" element={ <EditBook/> } />
         </Routes>
-        <Footer/>  
+        <Footer/>
       </Router>
     </>
   )
